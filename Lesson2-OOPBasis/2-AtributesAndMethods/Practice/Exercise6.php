a41ec7d4dc89f296c2538c578c7be9f3f27c26a0
Federico Bloomer Reeve <bloomerfede@gmail.com>
2021-01-16 13:22:52 -0300

    L2-AttrAndMethods-Ex 6 + L3-AbstractClasses3 done

--- Lesson2-OOPBasis/2-AtributesAndMethods/Practice/Exercise6.php
+++ Lesson2-OOPBasis/2-AtributesAndMethods/Practice/Exercise6.php
@@ -1,39 +1,46 @@
 <?php
+
 class Greeter {
 
-    private $name;
-    private $context;
+    private string $name;
+    private string $context;
+    private static array $validContexts = ['work', 'friends', 'newPerson'];
 
-    public function __construct(string $name, $context)
-    {
+    public function __construct(string $name, $context) {
         $this->name = $name;
-        $this->context = $context;
+        $this->setContext($context);
     }
 
-    public function setContext($context)
-    {
-        $this->context = $context;
+    public function setContext(string $context) {
+        if(in_array($context, static::$validContexts)) {
+            $this->context = $context;
+        } else {
+            echo $context . ' is not a valid context, let\'s say you\'re friends'."\n\n";
+            $this->context = static::$validContexts[2];
+        }
     }
 
-    private function giveInformalGreetings()
-    {
+    private function giveInformalGreetings() : string {
         return "Hi!, my name is " . $this->name;
     }
 
-    private function giveFormalGreetings()
-    {
+    private function giveFormalGreetings() : string {
         return "Hello, my name is " . $this->name;
     }
 
-    public function sayHello()
-    {
-        if ($this->context == 'work')
-        {
-            $greetings = $this->giveFormalGreetings();
-        } else {
-            $greetings = $this->giveInformalGreetings();
+    private function givePresentationGreetings() : string {
+        return "Nice to meet you, my name is " . $this->name . ", what's your name?";
+    }
+
+    public function sayHello() : string {
+        switch ($this->context) {
+            case 'work':
+                return $this->giveFormalGreetings();
+            case 'friends':
+                return $this->giveInformalGreetings();
+            case 'newPerson':
+                return $this->givePresentationGreetings();
         }
-        return $greetings;
     }
 }
 
@@ -42,18 +49,28 @@ echo $greeter->sayHello();
 echo "\n";
 $greeter->setContext("friends");
 echo $greeter->sayHello();
+echo "\n \n";;
+$greeter->setContext("newPerson");
+echo $greeter->sayHello();
+echo "\n \n";
+$greeter->setContext("newPerson");
+echo $greeter->sayHello();
+echo "\n \n";
+$greeter->setContext("sarasa");
+echo $greeter->sayHello();
 echo "\n \n";
 
 /*
  *
  * When changing the context from work to friends, what happened ?
  *
- * Your answer:
+ * Your answer: The private attribute is set to friends, when the sayHello method is called this attribute is analyzed
+ * and depending its value it will call the corresponding method which would be giveInformalGreetings
  *
  * When changing the context from work to friends, Did we change the behaviour of the class ?
  *
  * Your answer:
- *
+ * No, it's the same behaviour but it's adapted to have a different response according to the context provided.
  *
  * By following this design, add a new context. Commit the changes in the fork of your project
  *
--- Lesson3-Inheritance/AbstractClasses/Practice/AbstractClass3.php
+++ Lesson3-Inheritance/AbstractClasses/Practice/AbstractClass3.php
@@ -4,9 +4,59 @@
  *  Complete the classes below and add the necessary code for the example to work.
  */
 
-class Triangle {}
-class Square {}
-class Circle {}
+abstract class Figure {
+
+    public abstract function getArea();
+    public abstract function getPerimeter();
+}
+
+class Triangle extends Figure {
+    private array $sides;
+
+    public function __construct(int $height, int $base, int $hypotenuse) {
+        $this->sides = [$height, $base, $hypotenuse];
+    }
+
+    public function getArea() : int {
+        return ($this->sides[0] * $this->sides[1]) / 2;
+    }
+
+    public function getPerimeter() : int {
+        return $this->side1 + $this->side2 + $this->side3;
+    }
+}
+
+class Square extends Figure {
+    private int $side;
+
+    public function __construct(int $side) {
+        $this->side = $side;
+    }
+
+    public function getArea(): int {
+        return pow($this->side, 2);
+    }
+
+    public function getPerimeter(): int {
+        return $this->side * 4;
+    }
+}
+
+class Circle extends Figure {
+    private int $radius;
+
+    public function __construct(int $radius) {
+        $this->radius = $radius;
+    }
+
+    public function getArea(): float {
+        return M_PI * pow($this->radius, 2);
+    }
+
+    public function getPerimeter(): float {
+        return 2 * M_PI * $this->radius;
+    }
+}
 
 $triangle = new Triangle(2, 3, 5);
 $square = new Square(2);
